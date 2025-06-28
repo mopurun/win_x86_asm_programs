5b31af4af0e2fa276c1bfa570dd6e239c5f1908e
もぷるん(mopurun) <174941808+mopurun@users.noreply.github.com>
2025-06-28 19:45:42 +0900

    Add files via upload

---
+++ MessageBox1/hello.asm
@@ -0,0 +1,21 @@
+; hello.asm
+.586
+.model flat, stdcall
+ 
+NULL		EQU	0
+MB_OK		EQU	0
+ 
+MessageBoxA	proto :dword, :dword, :dword, :dword
+ExitProcess	proto :dword
+ 
+.data
+CAPTION		DB 'Hello!', 0
+TEXT		DB 'hello, world', 0
+ 
+.code
+mainCRTStartup	proc
+	invoke MessageBoxA, NULL, offset TEXT, offset CAPTION, MB_OK
+	invoke ExitProcess, 0
+	ret
+mainCRTStartup	endp
+end
---
+++ MessageBox1/make.bat
@@ -0,0 +1,5 @@
+cd C:\Program Files\Microsoft Visual Studio\2022\Community
+set destdir=%~dp0
+ml /Fo %destdir%hello.obj /c %destdir%hello.asm 
+link %destdir%hello.obj /OUT:%destdir%hello.exe /MACHINE:X86 /ENTRY:mainCRTStartup /SUBSYSTEM:CONSOLE /DYNAMICBASE "kernel32.lib" "user32.lib" "gdi32.lib" 
+
